d3e8644b80a4358648d2498ad499c2273fa974a2
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-11 21:09:25 +0600

    java program break throw

---
+++ Java/basics/src/Program.java
@@ -0,0 +1,16 @@
+public class Program {
+  public static void main(String[] args) {
+    System.out.println("java program break throw ");
+  }
+}
+/*
+ * public : public is an access modifier in java
+ * class: class is user defined blueprint
+ * Program: class name
+ * static: static allow to call methods without creating classes
+ * void : void is a return type it means null/nothing
+ * (String[] args) : its command line arguments
+ * System : System is a reserved class in java
+ * out : for output
+ * println : print anything in a new line
+ */
