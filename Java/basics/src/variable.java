ebd3446b32c4f5651880df80427f5ae61ab29853
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-11 21:19:13 +0600

    variables in java

---
+++ Java/basics/src/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+      System.out.println("variables in java");
+      int num = 10;
+      System.out.println(num);
+    }
+}
+/*
+ * variables: variables are container to store a value
+ * here:
+ *  int : int is a datatype
+ *  num : name of the variable
+ *  10 is assign value
+ */
