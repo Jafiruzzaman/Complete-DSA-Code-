cbe8e02176b000c82c688aa5aec849ad3672a48d
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-11 21:24:53 +0600

    rules of variables

--- Java/basics/src/variable.java
+++ Java/basics/src/variable.java
@@ -12,3 +12,11 @@ public class variable {
  *  num : name of the variable
  *  10 is assign value
  */
+/*
+ * Rules while creating variable:
+ * 1: variables name can't start with number or any special characters
+ * 2: variables name can't be a keyword
+ * 3: variables name can't contains space
+ * 4: variables name must be start with characters, dollar sign($) and underscore(_)
+ * 5: variables name should be meaningful
+ */
