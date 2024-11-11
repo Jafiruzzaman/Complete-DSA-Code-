57d04ae46d3bb7281005b5ea1a66ce92382ce570
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-11 21:38:18 +0600

    Primitive datatypes

---
+++ Java/basics/src/datatype.java
@@ -0,0 +1,43 @@
+public class datatype {
+  public static void main(String[] args) {
+    System.out.println("data types in java");
+    // byte
+    byte byteValue = 127;
+    System.out.println("byte value is "+byteValue);
+    // short
+    short shortestValue = 32000;
+    System.out.println("short value is "+shortestValue);
+    // int 
+    int intValue = 232142040;
+    System.out.println("int value is "+intValue);
+    // boolean
+    boolean isLoveJava = true;
+    System.out.println("boolean value is "+isLoveJava);
+    // char
+    char c = 'c';
+    System.out.println("char value is "+c);
+    // long
+    long longestValue = 2015412582145652040L;
+    System.out.println("long value is "+longestValue);
+    // double
+    double weight = 41.87d;
+    System.out.println("double value is "+weight);
+    // float
+    float floatValue = 5.4f;
+    System.out.println("float value is "+floatValue);
+
+  }
+}
+/*
+ * There are 2 two types of datatype
+ * 1: primitive
+ *  1: byte
+ *  2: short
+ *  3: int
+ *  4: boolean
+ *  5: char
+ *  6: long
+ *  7: double
+ *  8: float
+ * 1: non-primitive
+ */
