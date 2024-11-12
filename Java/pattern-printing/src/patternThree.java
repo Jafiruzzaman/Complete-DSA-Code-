ac0a98c4d787a40cd20a2a92a77d5ee1ae7f966c
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-12 19:21:47 +0600

    solve pattern problem 03

--- Java/pattern-printing/src/patternThree.java
+++ Java/pattern-printing/src/patternThree.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class patternThree {
+    public static void main(String[] args) {
+        System.out.println("print number rectangular pattern");
+        Scanner sc = new Scanner(System.in);
+//      row
+        System.out.println("Enter the number of row");
+        int row = sc.nextInt();
+//      col
+        System.out.println("Enter the number of col");
+        int col = sc.nextInt();
+//      print pattern
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
 }
