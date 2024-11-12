8df02ce083783aa1c7cda60b95c076e4c93677af
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-12 20:22:48 +0600

    solve pattern problem 11

---
+++ Java/pattern-printing/src/patternEleven.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class patternEleven {
+    public static void main(String[] args) {
+        System.out.println("print continuous character rectangular pattern");
+        Scanner sc = new Scanner(System.in);
+//      row
+        System.out.println("Enter number of row");
+        int row = sc.nextInt();
+//      col
+        System.out.println("Enter number of col");
+        int col = sc.nextInt();
+        char a = 'a';
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(a+" ");
+            a++;
+            }
+            System.out.println();
+        }
+    }
+}
