94d2d58e315a92e31fedafc7086f74669ed16309
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-11-12 14:22:04 +0600

    solve pattern problem 05

---
+++ Java/patterns/patternFive.java
@@ -0,0 +1,26 @@
+package Java.patterns;
+
+import java.util.Scanner;
+
+public class patternFive {
+  public static void main(String[] args) {
+    System.out.println("print number rectangular pattern ");
+    Scanner sc = new Scanner(System.in);
+    // row
+    System.out.println("Enter the number of row");
+    int row = sc.nextInt();
+    // col
+    System.out.println("Enter the number of col");
+    int col = sc.nextInt();
+    // print pattern
+    int count = 1;
+    for (int i = 0; i < row; i++) {
+      for (int j = 1; j <=col; j++) {
+        System.out.print(count+" ");
+        count++;
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
