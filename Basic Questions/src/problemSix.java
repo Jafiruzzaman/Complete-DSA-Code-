c9048ea750d282aafd07c576e02927ee8a7113ba
Jafiruzzaman Tuhin <168617233+Jafiruzzaman@users.noreply.github.com>
2024-11-13 22:58:31 +0600

    solve easy leetcode question 06

---
+++ Basic Questions/src/problemSix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class problemSix {
+    static boolean palindromeNumber(int x){
+        int reverse= 0;
+        int original= x;
+        if (x<0){
+            return false;
+        }
+        while (x>0){
+            int remainder= x%10;
+            reverse= reverse*10+remainder;
+            x/=10;
+        }
+        if (reverse==original){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("number palindrome");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int x = sc.nextInt();
+        boolean result = palindromeNumber(x);
+        System.out.println("The number is palindrome "+result);
+    }
+}
