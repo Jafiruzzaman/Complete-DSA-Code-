4a2f8140dc442e49389289b73d0e0d9c1be8dcd9
Jafiruzzaman Tuhin <168617233+Jafiruzzaman@users.noreply.github.com>
2024-11-13 22:00:27 +0600

    solve easy leetcode question 05

---
+++ Basic Questions/src/patternFive.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class patternFive {
+    static int squareRoot(int x){
+        int ans= 0;
+        if(x<2){
+            return 1;
+        }
+        for (int i = 1; i <=x ; i++) {
+            if (i*i==x){
+                ans = i;
+                return ans;
+            } else if (i*i>x) {
+                ans= i-1;
+                return ans;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int x = sc.nextInt();
+        int result = squareRoot(x);
+        System.out.println("The result is "+result);
+    }
+}
