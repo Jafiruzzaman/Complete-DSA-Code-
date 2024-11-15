f75148977291bde9cd9f1396dbda4a8a9cf169d3
Jafiruzzaman Tuhin <168617233+Jafiruzzaman@users.noreply.github.com>
2024-11-15 20:34:59 +0600

    Find the First Occurrence and solve question 04

---
+++ Binary Search/src/problemFour.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class problemFour {
+    static int FindFirstOccurrence(int[] arr,int target ){
+        int start = 0;
+        int end = arr.length-1;
+        int result =-1;
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if (arr[mid]==target){
+                result = mid;
+                end = mid-1;
+            } else if (arr[mid]>target) {
+                end = mid-1;
+            }else {
+                start=mid+1;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Find the first index of an element in a sorted Array ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter the array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println();
+        System.out.print("Enter the target value : ");
+        int target = sc.nextInt();
+        System.out.println("After implement binary search ");
+        int result = FindFirstOccurrence(arr,target);
+        System.out.println("The First Occurrence of "+target+" is "+result);
+    }
+}
