5befca0dfde31d668bf5d2fe5487c841574af5df
Jafiruzzaman Tuhin <168617233+Jafiruzzaman@users.noreply.github.com>
2024-11-16 11:12:51 +0600

    solve question how many times array is rotated

--- Binary Search/src/problemSeven.java
+++ Binary Search/src/problemSeven.java
@@ -1,5 +1,29 @@
 public class problemSeven {
-    public static void main(String[] args) {
-        System.out.println("how many times is a sorted array rotated");
+    static int rotatedArray(int[] arr){
+        int start =0;
+        int n = arr.length;
+        int end = n-1;
+        if (arr[start]<=arr[end]) {
+            return 0;
+        }
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            int prev = (mid+n-1)%n;
+            int next = (mid+1)%n;
+            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next]){
+                return mid;
+            }else if(arr[mid]>=arr[start]){
+                start = mid+1;
+        }else{
+            end=mid-1;
+        }
     }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {15, 18, 2, 3, 6, 12};
+        int rotationCount = rotatedArray(arr);
+        System.out.println("The array is rotated " + rotationCount + " times.");
+    }
+    
 }
